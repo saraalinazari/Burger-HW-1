5f1dfa841a963e8f133ed1659eda149c6bb168a5
Alex Edward Ball <ballalexedward@gmail.com>
2017-11-21 11:19:01 -0800

    db setup

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers (burger_name, devoured, date) VALUES ("Double Bacon Burger", FALSE, NOW());
+INSERT INTO burgers (burger_name, devoured, date) VALUES ("Protien Style Burger", FALSE, NOW());
+INSERT INTO burgers (burger_name, devoured, date) VALUES ("Korean BBQ Burger", FALSE, NOW());
+INSERT INTO burgers (burger_name, devoured, date) VALUES ("Gouda Cheese and Mushroom Burger", FALSE, NOW());

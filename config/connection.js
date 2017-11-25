75f1e6cb6b685186b463954ee5117419352c787d
Alex Edward Ball <ballalexedward@gmail.com>
2017-11-25 10:23:43 -0800

    fixed pooling syntax, pool to keep db connected

--- config/connection.js
+++ config/connection.js
@@ -15,7 +15,7 @@ const pool = mysql.createPool({
 //   database: "burgers_db"
 // });
 
-pool.connect(function(err) {
+pool.getConnection(function(err) {
   if (err) {
     console.error("error connecting: " + err.stack);
     return;

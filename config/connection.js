cc2167fcb3066a8aa74565155a58120ec0e98f94
Alex Edward Ball <ballalexedward@gmail.com>
2017-11-24 22:24:21 -0800

    cleardb

--- config/connection.js
+++ config/connection.js
@@ -1,18 +1,18 @@
 const mysql = require("mysql");
 
-// const connection = mysql.createConnection({
-//   host: "us-cdbr-iron-east-05.cleardb.net",
-//   user: "b4f2a1196d0130",
-//   password: '8fd9610d',
-//   database: "heroku_30aa0966782038e"
-// });
-
 const connection = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "",
-  database: "burgers_db"
+  host: "us-cdbr-iron-east-05.cleardb.net",
+  user: "b4f2a1196d0130",
+  password: '8fd9610d',
+  database: "heroku_30aa0966782038e"
 });
+
+// const connection = mysql.createConnection({
+//   host: "localhost",
+//   user: "root",
+//   password: "",
+//   database: "burgers_db"
+// });
 
 connection.connect(function(err) {
   if (err) {

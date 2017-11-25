288d3794d02a4df9d1c8a2b9b0759b7ab9681da2
Alex Edward Ball <ballalexedward@gmail.com>
2017-11-24 17:38:08 -0800

    changed db config for live site

--- config/connection.js
+++ config/connection.js
@@ -1,9 +1,9 @@
 const mysql = require("mysql");
 
 const connection = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "",
+  host: "us-cdbr-iron-east-05.cleardb.net",
+  user: "b4f2a1196d0130",
+  password: "8fd9610d",
   database: "burgers_db"
 });
 
